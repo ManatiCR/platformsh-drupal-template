e8736758bb484c7a3b00ce3a92fb56d6e19035ae
Marvin J. Cortés <marvin.cortes@gmail.com>
2021-02-22 21:47:17 -0600

    Update to latest upstream.

--- settings/settings.platformsh.php
+++ settings/settings.platformsh.php
@@ -28,6 +28,22 @@ if ($platformsh->hasRelationship('database')) {
   ];
 }
 
+// Enable verbose error messages on development branches, but not on the
+// production branch.
+// You may add more debug-centric settings here if desired to have them
+// automatically enable on development but not production.
+if (isset($platformsh->branch)) {
+  // Production type environment.
+  if ($platformsh->branch == 'master' || $platformsh->onDedicated()) {
+    $config['system.logging']['error_level'] = 'hide';
+  }
+  // Development type environment.
+  else {
+    $config['system.logging']['error_level'] = 'verbose';
+  }
+}
+
+
 // Enable Redis caching.
 if ($platformsh->hasRelationship('redis') && !InstallerKernel::installationAttempted() && extension_loaded('redis') && class_exists('Drupal\redis\ClientFactory')) {
   $redis = $platformsh->credentials('redis');
@@ -148,28 +164,28 @@ foreach ($platformsh->variables() as $name => $value) {
   $parts = explode(':', $name);
   list($prefix, $key) = array_pad($parts, 3, NULL);
   switch ($prefix) {
-    // Variables that begin with `d8settings` or `drupal` get mapped
-    // to the $settings array verbatim, even if the value is an array.
-    // For example, a variable named d8settings:example-setting' with
-    // value 'foo' becomes $settings['example-setting'] = 'foo';.
+      // Variables that begin with `d8settings` or `drupal` get mapped
+      // to the $settings array verbatim, even if the value is an array.
+      // For example, a variable named d8settings:example-setting' with
+      // value 'foo' becomes $settings['example-setting'] = 'foo';.
     case 'd8settings':
     case 'drupal':
       $settings[$key] = $value;
       break;
 
-    // Variables that begin with `d8config` get mapped to the $config
-    // array.  Deeply nested variable names, with colon delimiters,
-    // get mapped to deeply nested array elements. Array values
-    // get added to the end just like a scalar. Variables without
-    // both a config object name and property are skipped.
-    // Example: Variable `d8config:conf_file:prop` with value `foo` becomes
-    // $config['conf_file']['prop'] = 'foo';
-    // Example: Variable `d8config:conf_file:prop:subprop` with value
-    // `foo` becomes $config['conf_file']['prop']['subprop'] = 'foo';
-    // Example: Variable `d8config:conf_file:prop:subprop` with value
-    // ['foo' => 'bar'] becomes
-    // $config['conf_file']['prop']['subprop']['foo'] = 'bar';
-    // Example: Variable `d8config:prop` is ignored.
+      // Variables that begin with `d8config` get mapped to the $config
+      // array.  Deeply nested variable names, with colon delimiters,
+      // get mapped to deeply nested array elements. Array values
+      // get added to the end just like a scalar. Variables without
+      // both a config object name and property are skipped.
+      // Example: Variable `d8config:conf_file:prop` with value `foo` becomes
+      // $config['conf_file']['prop'] = 'foo';
+      // Example: Variable `d8config:conf_file:prop:subprop` with value
+      // `foo` becomes $config['conf_file']['prop']['subprop'] = 'foo';
+      // Example: Variable `d8config:conf_file:prop:subprop` with value
+      // ['foo' => 'bar'] becomes
+      // $config['conf_file']['prop']['subprop']['foo'] = 'bar';
+      // Example: Variable `d8config:prop` is ignored.
     case 'd8config':
       if (count($parts) > 2) {
         $temp = &$config[$key];
