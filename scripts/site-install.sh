4b753695e5024ca228951bfb20fb5800a92c8727
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2021-07-30 10:05:02 -0600

    Removing site uuid variable from the site install script

--- scripts/site-install.sh
+++ scripts/site-install.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-# Generate a new ramdom uuid the first time you create a project.
-# You could use the `uuidgen` bash command to get new one!!.
-SITE_UUID="e6191fd0-ba37-4233-a58f-6401649f27eb"
 chirripo drush cc drush
 echo "Installing the site..."
-chirripo drush si -- bloom --account-pass=admin --site-name="Drupal8" -y
-echo "Setting the site uuid..."
-chirripo drush config:set -- system.site uuid "$SITE_UUID" -y
-if [ -f ./config/sync/core.extension.yml ]; then chirripo drush cim -- -y; chirripo drush cim -- -y; fi
+chirripo drush si -- bloom --account-pass=admin --site-name="Drupal 9" --existing-config -y
+chirripo drush cim -- -y; chirripo drush cim -- -y;
 
 # Change CUSTOMTHEME by your own theme folder.
 if [ -f ./themes/custom/CUSTOMTHEME/package.json ]; then
